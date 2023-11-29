3086e9d286c0ca2d83d738dffe5fc5b5c0c3122c
tomlukrprogram <tomlukeprogramming@outlook.com>
2023-11-29 15:35:47 +0000

    started a new project lol

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Dashboard from './Components/Dash/Dashboard'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Dashboard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Campaigns/Campaigns.jsx
@@ -0,0 +1,12 @@
+
+const Campaigns = () => {
+return  (
+<main className="dash-main">
+<section className="campaigns-section">
+  <h3>Campaigns</h3>
+ 
+</section>
+</main>
+)
+}
+export default Campaigns
